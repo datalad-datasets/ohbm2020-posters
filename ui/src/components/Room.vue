ca79baaa33fc8dd9d7020c3ddf10bc40c6f748a6
Soichi Hayashi <hayashis@iu.edu>
2020-07-01 14:00:18 -0400

    keepalive now sends the actual participants counts from jitsi api

--- ui/src/components/Room.vue
+++ ui/src/components/Room.vue
@@ -17,42 +17,51 @@ export default {
             number: this.$route.params.number,
             cid: null, //will use jitsi client id
             wss: null, //websocket server connection
+            api: null, //jitsi api
             timer: null, //for keepalive
         }
     },
     mounted() {
-        const api = new JitsiMeetExternalAPI("meet.jit.si", {
+        this.api = new JitsiMeetExternalAPI("meet.jit.si", {
             roomName: this.$route.params.name,
             parentNode: document.querySelector('#meet'),
         })
-        api.addEventListener("videoConferenceJoined", e=>{
+        this.api.addEventListener("videoConferenceJoined", e=>{
             console.log("videoConferenceJoined .. starting keepalive");
             console.dir(e);
             this.cid = "datalad."+e.id; //let's use jitsi generated id
 
             this.wss = new ReconnectingWebSocket("wss://dev1.soichi.us/ohbm2020/");
             this.wss.onopen = () => {
-                this.wss.send(JSON.stringify({action: "jit", id: this.number, cid: this.cid}));
-                this.timer = setInterval(()=>{
-                    let people = api.getNumberOfParticipants(); //TODO - maybe we should report this?
-                    console.log(this.number, this.cid, "current participants", people);
-                    this.wss.send(JSON.stringify({action: "jit", id: this.number, cid: this.cid}));
-                }, 1000*30);
+                this.sendCount();
+                this.timer = setInterval(this.sendCount, 1000*30);
             }
         });
-        function sendClose() {
-        }
 
         //we *really* want the counter to go down!
-        api.addEventListener("videoConferenceLeft", this.sendClose);
-        api.addEventListener("readyToClose", this.sendClose);
+        this.api.addEventListener("videoConferenceLeft", this.sendClose);
+        this.api.addEventListener("readyToClose", this.sendClose);
         window.addEventListener("beforeunload", this.sendClose);
     },
     methods: {
+        sendCount() {
+            console.log(this.number, this.cid, "current participants", this.api.getNumberOfParticipants());
+            this.wss.send(JSON.stringify({
+                action: "jit", 
+                id: this.number, 
+                cid: this.cid, 
+                realCount: this.api.getNumberOfParticipants(),
+            }));
+        },
+
         sendClose() {
             console.log("closing!");
             clearInterval(this.timer);
-            this.wss.send(JSON.stringify({action: "jitclose", id: this.number, cid: this.cid}));
+            this.wss.send(JSON.stringify({
+                action: "jitclose", 
+                id: this.number, 
+                cid: this.cid
+            }));
         },
     },
 
